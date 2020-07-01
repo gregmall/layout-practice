3826768684c6c5118eef8884714eb1c43aea39ba
Greg Mall <gregmall157@gmail.com>
2020-06-30 17:11:51 -0700

    practice with layout

---
+++ app.js
@@ -0,0 +1,63 @@
+import {add, subtract, product, divide} from './utils.js';
+const addInput1 = document.getElementById('add-input-1');
+
+const addInput2 = document.getElementById('add-input-2');
+
+const addButton = document.getElementById('add-button');
+
+const addSpan = document.getElementById('add-result');
+
+addButton.addEventListener('click', () =>{
+    
+    const value1 = addInput1.value;
+    const value2 = addInput2.value;
+    console.log(value1);
+    console.log(value2);
+    const sum = add(value1, value2);
+    addSpan.textContent = sum;
+});
+
+const subInput1= document.getElementById('sub-input-1');
+const subInput2= document.getElementById('sub-input-2');
+const subButton= document.getElementById('sub-button');
+const subSpan=document.getElementById('sub-result');
+
+subButton.addEventListener('click', () =>{
+    const value1 = subInput1.value;
+    const value2 = subInput2.value;
+    console.log(value1);
+    console.log(value2);
+    const sub = subtract(value1, value2);
+    subSpan.textContent = sub;
+});
+
+const multInput1 = document.getElementById('mult-input-1');
+const multInput2 = document.getElementById('mult-input-2');
+const multButton = document.getElementById('mult-button');
+const multSpan= document.getElementById('mult-result');
+
+multButton.addEventListener('click', () =>{
+    const value1 = multInput1.value;
+    const value2 = multInput2.value;
+    console.log(value1);
+    console.log(value2);
+    const multiply = product(value1, value2);
+    multSpan.textContent = multiply;
+
+});
+
+const divInput1 = document.getElementById('div-input-1');
+const divInput2= document.getElementById('div-input-2');
+const divButton = document.getElementById('div-button');
+const divSpan = document.getElementById('div-result');
+
+divButton.addEventListener('click', ()=>{
+    const value1 = divInput1.value;
+    const value2 = divInput2.value;
+    console.log(value1);
+    console.log(value2);
+    const division = divide(value1, value2);
+    divSpan.textContent = division;
+});
+
+
---
+++ utils.js
@@ -0,0 +1,15 @@
+export function add(value1, value2){
+    return Number(value1) + Number(value2);
+}
+export function subtract(value1, value2){
+    return Number(value1) - Number(value2);
+
+}
+export function product(value1, value2){
+    return Number(value1) * Number(value2);
+
+}
+export function divide(value1, value2){
+    return Number(value1) / Number(value2);
+    
+}
